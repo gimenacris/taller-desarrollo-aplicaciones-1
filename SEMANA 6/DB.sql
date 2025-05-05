a281de8bebf5a26b23bd0800cfeee841e1cb0f6e
gimenacris <q00076h@upla.edu.pe>
2025-05-05 01:07:57 -0500

    Create DB.sql

---
+++ SEMANA 6/DB.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS gestion_carnets;
+USE gestion_carnets;
+
+CREATE TABLE IF NOT EXISTS carnets (
+    codigo VARCHAR(10) PRIMARY KEY,
+    dni VARCHAR(15) NOT NULL,
+    nombre_completo VARCHAR(100) NOT NULL,
+    facultad VARCHAR(100) NOT NULL,
+    carrera VARCHAR(100) NOT NULL
+);
